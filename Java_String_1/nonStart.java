e0e7de5a0f78fc30ea2ae8981d1db3c92b5d27c4
kbbp <26038255+kbbp@users.noreply.github.com>
2018-03-05 02:16:04 +0300

    Update nonStart.java

--- Java_String_1/nonStart.java
+++ Java_String_1/nonStart.java
@@ -1,13 +1,11 @@
 public String nonStart(String a, String b) {
   if (a.length() > 1) {
     a = a.substring(1);
-  }
-  else {a = "";}
+  } else {a = "";}
   
   if (b.length() > 1) {
     b = b.substring(1);
-  }
-  else {b = "";}
+  } else {b = "";}
   
   return a + b;
 }
