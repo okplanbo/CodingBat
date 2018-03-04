f314c34439e5e694e13db08efbbd42acd75bffd2
kbbp <26038255+kbbp@users.noreply.github.com>
2018-03-05 02:14:38 +0300

    Update right2.java

--- Java_String_1/right2.java
+++ Java_String_1/right2.java
@@ -2,8 +2,7 @@ public String right2(String str) {
   int l = str.length();
   if (l == 2) {
     return str;
-  }
-  else {
+  } else {
     return str.substring(l-2) + str.substring(0,l-2);
   }
 }
